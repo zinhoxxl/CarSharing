a66fb913fb4260e33bfe35205c11971a5a89e942
alpha <zinhoxxl@gmail.com>
2021-12-24 17:28:02 +0900

    1224

--- TEAM_1/src/command/QnABoard/QnABoardList.java
+++ TEAM_1/src/command/QnABoard/QnABoardList.java
@@ -9,7 +9,7 @@ public class QnABoardList implements MainCommand{
 
 	@Override
 	public String action(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
